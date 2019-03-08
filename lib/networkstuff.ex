1c55d00d6c26b9dc425f2841542586f65bc00ea5
jostlowe.com <jostlowe@gmail.com>
2019-03-08 17:22:53 +0100

    added more networkstuff

--- lib/networkstuff.ex
+++ lib/networkstuff.ex
@@ -6,26 +6,34 @@ defmodule NetworkStuff do
 
 
   @doc """
-  Returns (hopefully) the ip address of you network interface. 
+  Returns (hopefully) the ip address of your network interface. 
 
   ## Examples
-      iex> Kokeplata.get_all_button_types
-      [:hall_up, :hall_down, :cab]
+      iex> NetworkStuff.get_my_ip
+      {10, 100, 23, 253}
   """
 
   def get_my_ip do
     {:ok, socket} = :gen_udp.open(6789, [active: false, broadcast: true])
     :ok = :gen_udp.send(socket, {255,255,255,255}, 6789, "test packet")
     ip = case :gen_udp.recv(socket, 100, 1000) do
-      {:ok, {ip, _port, _data}} -> {:ok, ip}
+      {:ok, {ip, _port, _data}} -> ip
       {:error, _} -> {:error, :could_not_get_ip}
     end
     :gen_udp.close(socket)
     ip
   end
 
+
+
+  @doc """
+  formats an ip address on tuple format to a bytestring
+  ## Examples
+      iex> NetworkStuff.ip_to_string {10, 100, 23, 253}
+      '10.100.23.253'
+  """
+
   def ip_to_string ip do
     :inet.ntoa ip
   end
-  
 end
