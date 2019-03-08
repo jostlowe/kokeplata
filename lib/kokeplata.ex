2ca9108a9c068d87a057a312acf79bf61fbc23ba
jostlowe.com <jostlowe@gmail.com>
2019-03-08 16:43:48 +0100

    added some fun button functions

--- lib/kokeplata.ex
+++ lib/kokeplata.ex
@@ -1,18 +1,66 @@
 defmodule Kokeplata do
   @moduledoc """
-  Documentation for Kokeplata.
+  A bunch o' little titbits of code that may (or may not) make the elevator lab slightly more survivable
   """
+
 
   @doc """
-  Hello world.
+  Returns all the different types of buttons on the elevator panel.
 
   ## Examples
-
-      iex> Kokeplata.hello()
-      :world
-
+      iex> Kokeplata.get_all_button_types
+      [:hall_up, :hall_down, :cab]
   """
-  def hello do
-    :world
+
+  def get_all_button_types do
+    [:hall_up, :hall_down, :cab]
+  end
+
+
+
+  @doc """
+  Returns all possible orders of a single button type, given the number of the top floor
+  Returns a list of tuples on the from {button_type, floor}
+
+  ## Examples
+      iex> Kokeplata.get_all_button_types(:cab, 3)
+      [cab: 0, cab: 1, cab: 2, cab: 3]
+      iex> Kokeplata.get_all_button_types(:hall_up, 3)
+      [hall_up: 0, hall_up: 1, hall_up: 2]
+  """
+
+  def get_buttons_of_type(button_type, top_floor) do
+    floor_list = case button_type do
+      :hall_up -> 0..top_floor-1
+      :hall_down -> 1..top_floor
+      :cab -> 0..top_floor
+    end
+    floor_list |> Enum.map(fn floor -> {button_type, floor} end)
+  end
+
+
+
+  @doc """
+  Returns all possible orders on a single elevator
+  Returns a list of tuples on the from {button_type, floor}
+
+  ## Examples
+      iex> Kokeplata.get_all_buttons(3)
+      [
+        hall_up: 0,
+        hall_up: 1,
+        hall_up: 2,
+        hall_down: 1,
+        hall_down: 2,
+        hall_down: 3,
+        cab: 0,
+        cab: 1,
+        cab: 2,
+        cab: 3
+      ]
+  """
+
+  def get_all_buttons(top_floor) do
+    get_all_button_types() |> Enum.map(fn button_type -> get_buttons_of_type(button_type, top_floor) end) |> List.flatten
   end
 end
